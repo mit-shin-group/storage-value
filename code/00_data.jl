bdb5226bc45f07dd10e53e76f48cb2b6883db637
lauinger <lauinger@mit.edu>
2025-01-03 15:22:30 +0100

    add joint planning and operating problem

--- code/00_data.jl
+++ code/00_data.jl
@@ -1,6 +1,6 @@
 using Parameters
 
-@with_kw struct CaseData
+@with_kw struct CaseDataBO
     Δt::Float64 = 1.
     p̄::Float64 = 10.
     p::Vector{Float64} = [1., 1., 1.]
@@ -11,4 +11,25 @@ using Parameters
     ȳ::Float64 = 8.
     ηc::Float64 = 0.92
     ηd::Float64 = 0.92
+end
+
+@with_kw struct CaseData
+    Nb::Int64 = 1
+    Nℓ::Int64 = 2
+    l̄0::Vector{Float64} = [0., 0.]
+    x̄b0::Vector{Float64} = [0., 0.]
+    pb::Vector{Float64} = [1., 1.]
+    pℓ::Vector{Float64} = [0.5, 0.5]
+    x̲ℓ::Float64 = 2.
+    x̄ℓ::Float64 = 2.
+    x̲b::Float64 = 0.
+    x̄b::Float64 = 2.
+    p̄::Float64 = 10.
+    p::Vector{Vector{Float64}} = [[1., 1., 1.], [1., 1., 1.]]
+    ℓ::Vector{Vector{Float64}} = [[1., 2., 1.], [1.25, 2.5, 1.25]]
+    ηc::Float64 = 0.92
+    ηd::Float64 = 0.92
+    Tn::Float64 = 1/length(p[1])
+    Ts::Float64 = 8.
+    Δt::Float64 = 1.
 end
---
+++ code/06_joint_problem.jl
@@ -0,0 +1,59 @@
+using JuMP, Gurobi
+include("00_data.jl")
+
+# lifetime functions
+function n̲(n::Int, N::Int)
+    return max(1, n - N + 1)
+end
+
+function build_JP(; case_data::CaseData = CaseData())
+    # unpack data
+    @unpack Nb, Nℓ, l̄0, x̄b0, pb, pℓ, x̲ℓ, x̄ℓ, x̲b, x̄b, p̄, p, ℓ, ηc, ηd, Tn, Ts, Δt = case_data;
+    K = length(p[1])
+    N = length(pb) 
+    # build battery operation Model
+    JP = Model()
+    # add variables
+    # -- planning
+    @variable(JP, xb[1:N] >= 0.)
+    @variable(JP, xℓ[1:N] >= 0.)
+    @variable(JP, zb[1:N], Bin)
+    @variable(JP, zℓ[1:N], Bin) 
+    # -- operational
+    @variable(JP, xc[1:N, 1:K] >= 0.)
+    @variable(JP, xd[1:N, 1:K] >= 0.)
+    @variable(JP, y0[1:N] >= 0.)
+    @variable(JP, s[1:N, 1:K] >= 0.)
+    # add constraints
+    # -- planning
+    @constraint(JP, zb .* x̲b .<= xb)
+    @constraint(JP, xb .<= zb .* x̄b)
+    @constraint(JP, zℓ .* x̲ℓ .<= xℓ)
+    @constraint(JP, xℓ .<= zℓ .* x̄ℓ)
+    # -- operational
+    @constraint(JP, [n = 1:N, k = 1:K], xc[n,k] - xd[n,k] <= x̄b0[n] + sum( xb[i] for i in n̲(n, Nb):n ))
+    @constraint(JP, [n = 1:N, k = 1:K], xc[n,k] - xd[n,k] >= -x̄b0[n] - sum( xb[i] for i in n̲(n, Nb):n ))
+    @constraint(JP, [n = 1:N, k = 1:K], y0[n] + Δt * sum( ηc * xc[n,l] - xd[n,l]/ηd for l = 1:k) <= Ts * (x̄b0[n] + sum( xb[i] for i in n̲(n, Nb):n )) )
+    @constraint(JP, [n = 1:N, k = 1:K-1], y0[n] + Δt * sum( ηc * xc[n,l] - xd[n,l]/ηd for l = 1:k) >= 0.)
+    @constraint(JP, [n = 1:N], sum( ηc * xc[n,l] - xd[n,l]/ηd for l = 1:K) >= 0)
+    # add objective
+    @constraint(JP, [n = 1:N, k=1:K], s[n,k] >= xc[n,k] - xd[n,k] + ℓ[n][k] - l̄0[n] - sum( xℓ[i] for i in n̲(n, Nℓ):n ))
+    @objective(JP, Min, sum( pb[n] * xb[n] + pℓ[n] * xℓ[n] + Tn * sum( p̄ * s[n,k] + p[n][k] * (xc[n,k] - xd[n,k]) for k = 1:K ) for n in 1:N))
+    return JP
+end
+
+function solve_JP(; case_data::CaseData = CaseData())
+    JP = build_JP(case_data = case_data)
+    set_optimizer(JP, Gurobi.Optimizer)
+    optimize!(JP)
+    return JP
+end
+
+function printsol(model::Model)
+    println("Objective value: ", round(objective_value(model), digits = 3))
+    println("...")
+    println("Optimal decisions:") 
+    for var in all_variables(model) 
+        println("$(name(var)) = $(round(value(var), digits = 3))")
+    end
+end
