ce9792e0e1f411a2012e561b5aea0c1599a67d26
lauinger <lauinger@mit.edu>
2025-01-23 17:02:47 -0500

    2024 data analysis

--- code/02_isone_data.py
+++ code/02_isone_data.py
@@ -114,8 +114,10 @@ def download_data(yr, mh):
     # Close the browser
     driver.quit()
     
-year_list = ["2022", "2021", "2020", "2019", "2018", "2017"]
-month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+# year_list = ["2022", "2021", "2020", "2019", "2018", "2017"]
+# month_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+year_list = ["2024"]
+month_list = ["Sep", "Oct", "Nov", "Dec"]
 
 for yr in year_list:
     for mh in month_list:
